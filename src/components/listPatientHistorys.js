5e39976208d1c059f5e5101cca2edea244412751
Duc Anh <tdanhdev@gmail.com>
2018-09-29 17:32:14 +0700

    Add list component

---
+++ src/components/listPatientHistorys.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+
+export default class ListPatientHistory extends Component {
+    render() {
+        return (
+            <div>
+                <main id="listar-main" className="listar-main listar-innerspeace listar-bglight listar-haslayout" style={{ textAlign: 'left' }}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <div id="listar-content" className="listar-content" style={{ marginTop: '-50px' }}>
+                                    <div id="listar-testimonials" className="listar-testimonials listar-testimonialsvtwo">
+                                        <div className="listar-testimonial" style={{ width: '60%', marginLeft: '19%' }}>
+                                            <div className="listar-testimonialholder">
+                                                <span class="listar-iconquote"><img src="images/icons/icon-06.png" alt="image description" /></span>
+                                                <div className="listar-reviewcontent">
+                                                    <div className="listar-reviewhead">
+                                                        <div className="listar-reviewheading">
+                                                            <h3><a href="javascript:void(0);">0xb05dbba60ba029be2d4fca9152ff720ce7f2697e</a></h3>
+                                                        </div>
+                                                        <div className="row" style={{ paddingTop: '10px' }}>
+                                                            <div className="col-md-6">
+                                                                <p><i className="fas fa-birthday-cake">&nbsp;Date of birth: 13/01/1997</i></p>
+                                                                <p><i className="fas fa-transgender">&nbsp;Gender: Male</i></p>
+                                                            </div>
+                                                            <div className="col-md-6">
+                                                                <p><i class="fas fa-user-md" title="asdasdsa">&nbsp;Doctor: Dr. Tung</i></p>
+                                                                <p><i class="fas fa-hospital">&nbsp;Hospital: Cho Ray</i></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="listar-description" style={{ padding: '0' }}>
+                                                        <i className="far fa-clock" style={{ color: 'rgb(109, 105, 105)' }}>&nbsp;IN: 29/09/2018</i>
+                                                        <i className="far fa-clock" style={{ marginLeft: '20px', color: "rgb(109, 105, 105)" }}>&nbsp;OUT: 29/10/2018</i>
+                                                    </div>
+                                                    <br />
+                                                    <hr style={{ clear: 'both' }} />
+                                                    {/*------*/}
+                                                    <div className="listar-reviewhead">
+                                                        <div className="listar-reviewheading">
+                                                            <h3><a href="javascript:void(0);">0xb05dbba60ba029be2d4fca9152ff720ce7f2697e</a></h3>
+                                                        </div>
+                                                        <div className="row" style={{ paddingTop: '10px' }}>
+                                                            <div className="col-md-6">
+                                                                <p><i className="fas fa-birthday-cake">&nbsp;Date of birth: 13/01/1997</i></p>
+                                                                <p><i className="fas fa-transgender">&nbsp;Gender: Male</i></p>
+                                                            </div>
+                                                            <div className="col-md-6">
+                                                                <p><i class="fas fa-user-md" title="asdasdsa">&nbsp;Doctor: Dr. Tung</i></p>
+                                                                <p><i class="fas fa-hospital">&nbsp;Hospital: Cho Ray</i></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="listar-description" style={{ padding: '0' }}>
+                                                        <i className="far fa-clock" style={{ color: 'rgb(109, 105, 105)' }}>&nbsp;IN: 29/09/2018</i>
+                                                        <i className="far fa-clock" style={{ marginLeft: '20px', color: "rgb(109, 105, 105)" }}>&nbsp;OUT: 29/10/2018</i>
+                                                    </div>
+                                                    <br />
+                                                    <hr style={{ clear: 'both' }} />
+                                                    {/*------*/}
+                                                    <div className="listar-reviewhead">
+                                                        <div className="listar-reviewheading">
+                                                            <h3><a href="javascript:void(0);">0xb05dbba60ba029be2d4fca9152ff720ce7f2697e</a></h3>
+                                                        </div>
+                                                        <div className="row" style={{ paddingTop: '10px' }}>
+                                                            <div className="col-md-6">
+                                                                <p><i className="fas fa-birthday-cake">&nbsp;Date of birth: 13/01/1997</i></p>
+                                                                <p><i className="fas fa-transgender">&nbsp;Gender: Male</i></p>
+                                                            </div>
+                                                            <div className="col-md-6">
+                                                                <p><i class="fas fa-user-md" title="asdasdsa">&nbsp;Doctor: Dr. Tung</i></p>
+                                                                <p><i class="fas fa-hospital">&nbsp;Hospital: Cho Ray</i></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="listar-description" style={{ padding: '0' }}>
+                                                        <i className="far fa-clock" style={{ color: 'rgb(109, 105, 105)' }}>&nbsp;IN: 29/09/2018</i>
+                                                        <i className="far fa-clock" style={{ marginLeft: '20px', color: "rgb(109, 105, 105)" }}>&nbsp;OUT: 29/10/2018</i>
+                                                    </div>
+                                                    <br />
+                                                    <hr style={{ clear: 'both' }} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main >
+            </div >
+        )
+    }
+}
